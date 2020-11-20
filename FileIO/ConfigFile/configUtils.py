4b4a70135dfd92c4c9f0e3c981f7e07283a7ebaf
Michael Zapata <mikeyfriends@hotmail.com>
2020-11-20 14:20:11 +0000

    added configparser

---
+++ FileIO/ConfigFile/configUtils.py
@@ -0,0 +1,75 @@
+import configparser
+import os
+
+FONT = "Courier"
+FONT_SIZE = 8
+
+def createConfig(path):
+    """
+    Create a config file
+    """
+
+    config = configparser.ConfigParser()
+
+    config.add_section("Settings") # section heading - eg: [Settings]
+    # .set(HEADING, KEY, VALUE)
+    config.set("Settings", "font", FONT) # font = Courier
+    config.set("Settings", "font_size", "10")
+    config.set("Settings", "font_style", "Normal")
+    config.set("Settings", "font_info", f"You are using {FONT}s at {FONT_SIZE} pt")
+
+    with open(path, "w") as config_file:
+        config.write(config_file)
+
+def get_config(path):
+    """
+    Returns the config object
+    """
+    #create config only if it doesn't exist;
+    if not os.path.exists(path):
+        createConfig(path)
+
+    config = configparser.ConfigParser()
+    config.read(path)
+    return config
+
+def get_setting(path, section, setting):
+    """
+    Get a setting
+    """
+    config = get_config(path)
+    value = config.get(section, setting)
+    return value
+
+def set_or_update_setting(path, section, setting, value):
+    """
+    Update a setting
+    """
+    config = get_config(path)
+    config.set(section, setting, value)
+    with open(path, "w") as config_file:
+        config.write(config_file)
+
+def delete_setting(path, section, setting):
+    """
+    Delete a setting
+    """
+    config = get_config(path)
+    config.remove_option(section, setting)
+    with open(path, "w") as config_file:
+        config.write(config_file)
+
+if __name__ == "__main__":
+    #--------------------------------------------------------
+    current_dir = os.path.dirname(__file__)
+    PATH = os.path.join(current_dir, 'settings.ini')
+    #--------------------------------------------------------
+
+    font = get_setting(PATH, 'Settings', 'font')
+    font_size = get_setting(PATH, 'Settings', 'font_size')
+
+    set_or_update_setting(PATH, "Settings", "font_size", "12")
+    font_size = get_setting(PATH, 'Settings', 'font_size')
+
+    delete_setting(PATH, "Settings", "font_style")
+    font = get_setting(PATH, 'Settings', 'font_style')
